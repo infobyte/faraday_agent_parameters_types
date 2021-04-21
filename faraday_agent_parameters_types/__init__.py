212259436563fd9e44e8184641eff66ddf87463b
Nahuel Alonso <nalonso@infobytesec.com>
2021-04-21 13:41:48 -0300

    Manifest folder path

--- faraday_agent_parameters_types/__init__.py
+++ faraday_agent_parameters_types/__init__.py
@@ -3,3 +3,11 @@
 __author__ = """Blas Moyano"""
 __email__ = 'bmoyano@infobytesec.com'
 __version__ = '0.1.0'
+
+from pathlib import Path
+from typing import Union
+
+
+def manifests_folder() -> Union[Path, str]:
+
+    return Path(__file__).parent / "static" / "executors" / "official"
